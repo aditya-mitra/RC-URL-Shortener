1c396b0a7f73c0e256c16214de34feba44c34bab
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-04-26 18:29:53 +0530

    added a url shorten command

---
+++ src/UrlShortenCommand.ts
@@ -0,0 +1,31 @@
+import { IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import {
+  ISlashCommand,
+  SlashCommandContext,
+} from '@rocket.chat/apps-engine/definition/slashcommands';
+
+export default class UrlShortenCommand implements ISlashCommand {
+  public command = 'shorten-url';
+
+  public i18nDescription = 'shorten long urls';
+
+  public i18nParamsExample = '<url> <quick|custom>';
+
+  public providesPreview = false;
+
+  public async executor(
+    ctx: SlashCommandContext,
+    _read: IRead,
+    modify: IModify,
+  ): Promise<void> {
+    const message = 'shorten urls';
+
+    const messageStructure = await modify.getCreator().startMessage();
+    const sender = ctx.getSender(); // the user calling the slashcommand
+    const room = ctx.getRoom(); // the current room
+
+    messageStructure.setSender(sender).setRoom(room).setText(message);
+
+    await modify.getCreator().finish(messageStructure);
+  }
+}
