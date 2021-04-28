e4f039c7d8b0225200cdbcd586f19fcb188c23a1
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-04-28 17:57:23 +0530

    restore changes

--- src/zeroConfig/cleanUri.ts
+++ src/zeroConfig/cleanUri.ts
@@ -1,6 +1,6 @@
-import { IHttp } from "@rocket.chat/apps-engine/definition/accessors";
+import { IHttp } from '@rocket.chat/apps-engine/definition/accessors';
 
-import { IZeroConfigAPIResult } from "../types/zeroConfig";
+import { IZeroConfigAPIResult } from '../types/zeroConfig';
 
 /**
  * using cleanuri api
@@ -8,14 +8,14 @@ import { IZeroConfigAPIResult } from "../types/zeroConfig";
  */
 export default async function cleanUri(
   url: string,
-  http: IHttp
+  http: IHttp,
 ): Promise<IZeroConfigAPIResult> {
-  const resp = await http.post("https://cleanuri.com/api/v1/shorten", {
+  const resp = await http.post('https://cleanuri.com/api/v1/shorten', {
     data: { url },
   });
 
   const shortened = resp.data?.result_url;
   const error = resp.data?.error;
 
-  return { shortened, error, api: "cleanuri" };
+  return { shortened, error, api: 'cleanuri' };
 }
