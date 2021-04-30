07210a746cc7128f14176b09cacaad10c763124a
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-04-30 17:44:19 +0530

    add a primary button on notify message but message text is absent

--- src/lib/sendNotifyMessage.ts
+++ src/lib/sendNotifyMessage.ts
@@ -1,7 +1,14 @@
-import { INotifier } from '@rocket.chat/apps-engine/definition/accessors';
-import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
-import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
-import { IUser } from '@rocket.chat/apps-engine/definition/users';
+import { INotifier } from "@rocket.chat/apps-engine/definition/accessors";
+import {
+  IMessage,
+  MessageActionType,
+} from "@rocket.chat/apps-engine/definition/messages";
+import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
+import {
+  BlockType,
+  ButtonStyle,
+} from "@rocket.chat/apps-engine/definition/uikit";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
 
 interface IAlertMessage {
   notify: INotifier;
@@ -21,7 +28,25 @@ export default async function sendNotifyMessage({
     room,
     sender, // TODO: change the sender to app and not user
     avatarUrl:
-      'https://res.cloudinary.com/gamersinstinct7/image/upload/v1619461323/rc/url_shortener_icon.png', // TODO: change this to netlify later
+      "https://res.cloudinary.com/gamersinstinct7/image/upload/v1619461323/rc/url_shortener_icon.png", // TODO: change this to netlify later
+    blocks: [
+      {
+        type: BlockType.ACTIONS,
+        blockId: "abc",
+        // @ts-ignore
+        elements: [
+          {
+            type: MessageActionType.BUTTON,
+            text: {
+              type: "plain_text",
+              text: "is it visible?",
+            },
+            actionId: "abc",
+            style: ButtonStyle.PRIMARY,
+          },
+        ],
+      },
+    ],
   };
   await notify.notifyUser(sender, message);
 }
