1db8191ad35eeabd0fb8bdebd4afd592a883851e
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-05-01 15:28:11 +0530

    redirect to longurl

--- src/domainConfig/endpoint.ts
+++ src/domainConfig/endpoint.ts
@@ -16,10 +16,10 @@ import {
   RocketChatAssociationRecord,
 } from "@rocket.chat/apps-engine/definition/metadata";
 
-function respondWithBadRequest(): IApiResponse {
+function respondWithError(message?: string): IApiResponse {
   return {
     status: HttpStatusCode.BAD_REQUEST,
-    content: "The URL could not be found.",
+    content: message || "The URL could not be found.",
     headers: {
       "Content-Type": "Text/Plain",
     },
@@ -40,7 +40,7 @@ export default class RedirectEndpoint extends ApiEndpoint {
 
     const slug = req.params?.slug;
     if (!slug) {
-      return respondWithBadRequest();
+      return respondWithError();
     }
 
     const association = new RocketChatAssociationRecord(
@@ -51,14 +51,19 @@ export default class RedirectEndpoint extends ApiEndpoint {
     const found = await persistRead.readByAssociation(association);
 
     if (found.length === 0) {
-      return respondWithBadRequest();
+      return respondWithError();
     }
 
-    const content = found;
+    const longUrl = (found as any)[0].url;
+    if (!longUrl) {
+      return respondWithError(
+        "Unknown error in URL Shortener App.\nPlease check logs."
+      );
+    }
 
     return {
-      status: HttpStatusCode.OK,
-      content,
+      status: HttpStatusCode.FOUND,
+      headers: { Location: longUrl },
     };
   }
 }
