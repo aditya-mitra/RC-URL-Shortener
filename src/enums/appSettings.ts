767adf81c13387b0b70bdcae17abb0f7f494540c
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-05-01 17:10:31 +0530

    provide setting for domain url

--- src/enums/appSettings.ts
+++ src/enums/appSettings.ts
@@ -1,29 +1,33 @@
 export enum sections {
-  zero = 'zero config',
-  custom = 'custom config',
-  domain = 'domain name config',
+  zero = "zero config",
+  custom = "custom config",
+  domain = "domain name config",
 }
 
 export enum configTypes {
-  id = 'config_types',
-  zero = 'zero_config_enabled',
-  custom = 'custom_config_enabled',
-  domain = 'domain_name_config_enabled',
+  id = "config_types",
+  zero = "zero_config_enabled",
+  custom = "custom_config_enabled",
+  domain = "domain_name_config_enabled",
 }
 
 export enum zeroConfigs {
-  id = 'zero_config_provider',
-  cleanuri = 'cleanuri',
-  tinyuid = 'tinyuid',
-  shrtcode = 'shrtcode',
+  id = "zero_config_provider",
+  cleanuri = "cleanuri",
+  tinyuid = "tinyuid",
+  shrtcode = "shrtcode",
 }
 
 export enum customConfigs {
-  id = 'custom_config',
-  provider = 'custom_config_provider',
-  header = 'custom_config_header',
-  body = 'custom_config_post_request_body',
-  urlKey = 'long_url_key',
-  responseUrlKey = 'short_url_key',
-  statsEndpoint = 'stat_endpoint',
+  id = "custom_config",
+  provider = "custom_config_provider",
+  header = "custom_config_header",
+  body = "custom_config_post_request_body",
+  urlKey = "long_url_key",
+  responseUrlKey = "short_url_key",
+  statsEndpoint = "stat_endpoint",
+}
+
+export enum domainConfigs {
+  url = "domain_url",
 }
